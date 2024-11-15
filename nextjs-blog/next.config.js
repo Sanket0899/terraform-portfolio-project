1526724cd76f64ec030aee45cf9cfaa7935093cf
Sanket <sanketn767@gmail.com>
2024-11-15 09:13:45 +0530

    Initial commit of Next.js portfolio starter kit

---
+++ nextjs-blog/next.config.js
@@ -0,0 +1,14 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+    output: 'export',
+}
+
+ module.exports = nextConfig
+
+
+/** The next.config.js file in Next.js is used to configure 
+ * and customize the default behavior of a Next.js application. 
+ * It allows 
+ * developers to modify various settings to suit their needs, without changing the core Next.js code. */
